6e025d3453ff547418b0c685cf7b22ebecc17f5f
Relinton Pinheiro <relinton.pinheiro.infouniron@gmail.com>
2023-04-01 13:09:47 -0400

    Super mário, treinamento Css e JavaScript

---
+++ js/site.js
@@ -0,0 +1,32 @@
+﻿const mario = document.querySelector('.mario');
+const cano = document.querySelector('.cano');
+
+const pulo = () => {
+    mario.classList.add('pulo');
+
+    setTimeout(() => {
+        mario.classList.remove('pulo');
+    }, 500);
+}
+
+const loop = setInterval(() => {
+
+    const posicaoDoCano = cano.offsetLeft;
+    const posicaoDoMario = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if (posicaoDoCano <= 120 && posicaoDoCano > 0 && posicaoDoMario < 80) {
+        cano.style.animation = 'none';
+        cano.style.left = `${posicaoDoCano}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${posicaoDoMario}px`;
+
+        mario.src = './imagens/gamerOver.png';
+        mario.style.marginLeft = '5px';
+
+        clearInterval(loop);
+    }
+
+}, 10);
+
+document.addEventListener('keydown', pulo)
